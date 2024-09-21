31e8ceb41d1173893a39d54e18ffe901053a2f75
344_09 <344_09@344-09>
2024-09-21 14:49:48 +0700

    Add project files.

---
+++ Sult-you/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Sult_you
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.com = new System.Windows.Forms.ComboBox();
+            this.listshow = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // com
+            // 
+            this.com.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.com.FormattingEnabled = true;
+            this.com.Location = new System.Drawing.Point(177, 106);
+            this.com.Name = "com";
+            this.com.Size = new System.Drawing.Size(185, 39);
+            this.com.TabIndex = 0;
+            this.com.SelectedIndexChanged += new System.EventHandler(this.com_SelectedIndexChanged);
+            // 
+            // listshow
+            // 
+            this.listshow.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listshow.FormattingEnabled = true;
+            this.listshow.ItemHeight = 31;
+            this.listshow.Location = new System.Drawing.Point(177, 151);
+            this.listshow.Name = "listshow";
+            this.listshow.Size = new System.Drawing.Size(185, 376);
+            this.listshow.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Sult_you.Properties.Resources.ท่อง;
+            this.pictureBox1.Location = new System.Drawing.Point(127, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 89);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Sult_you.Properties.Resources.Screenshot_2024_08_01_094803_removebg_preview;
+            this.pictureBox2.Location = new System.Drawing.Point(262, 11);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 89);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Sult_you.Properties.Resources.images__3__removebg_preview;
+            this.pictureBox3.Location = new System.Drawing.Point(385, 11);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 89);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Sult_you.Properties.Resources.MOMDIE;
+            this.ClientSize = new System.Drawing.Size(711, 539);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listshow);
+            this.Controls.Add(this.com);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "กูท่องสูตรคูณได้";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox com;
+        private System.Windows.Forms.ListBox listshow;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Sult-you/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sult_you
+{
+    public partial class Form1 : Form
+    {
+        int mun;
+        int result;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listshow.Items.Clear();
+            for ( int i = 1; i <= 1000; i++)
+
+            {
+                com.Items.Add(i);
+            }
+        }
+
+        private void com_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            mun = Convert.ToInt16(com.SelectedItem);
+            listshow.Items.Clear();
+            for (int i = 1; i <=12 ; i++){
+                result = mun * i;
+                listshow.Items.Add(mun+"x"+i+"="+result);
+            }
+        }
+    }
+}
